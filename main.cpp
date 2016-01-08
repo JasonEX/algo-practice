67faadc47b66828c534ef431ff4e317fabed7c70
Yuqian <yuqian.yang@lusis.fr>
2016-01-08 16:22:46 +0100

    add more tests

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include "skipList.h"
 #include <time.h>
+using namespace std;
 
 int main()
 {
   srand(time(NULL));
   SkipList<int, int> list;
-  for (int i = 1; i < 100; i++)
+  for (int i = 1; i <= 100; i++)
     list.insert(createNode(i, 10));
-  for (int i = 1; i < 100; i += 2)
+  for (int i = 1; i <= 100; i += 2)
     list.remove(i);
+  //list.compact();
   list.dump();
+  cout << list.size() << endl;
 
   int ret = -1;
   if (list.find(10, ret))
